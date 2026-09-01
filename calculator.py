cba88083f2557edacdd8dbe639286c4a18d42529
giovanniarmas02 <giovanniarmasd@gmail.com>
2026-09-01 10:44:42 +0100

    Version 1.1: added while loop.

--- calculator.py
+++ calculator.py
@@ -17,15 +17,19 @@ def division(a,b):
     except ZeroDivisionError:
         return "Error: cannot divide by 0."
 
-num1=float(input("Enter the first number: "))
-num2=float(input("Enter the second number: "))
-operation=input("Choose operation(+, -, *, /): ")
-
-if operation=="+":
-    print(add(num1, num2))
-elif operation=="-":
-    print(subtract(num1, num2))
-elif operation=="*":
-    print(multiply(num1, num2))    
-elif operation=="/":
-    print(division(num1, num2))
+while True:
+    num1=float(input("Enter the first number: "))
+    num2=float(input("Enter the second number: "))
+    operation=input("Choose operation(+, -, *, /): ")
+    if operation=="+":
+        print(add(num1, num2))
+    elif operation=="-":
+        print(subtract(num1, num2))
+    elif operation=="*":
+        print(multiply(num1, num2))    
+    elif operation=="/":
+        print(division(num1, num2))
+    ask=input("Do you want to calculate again? (y/n)")
+    if ask !="y":
+        break
+    
