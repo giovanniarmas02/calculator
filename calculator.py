914fe714d14a28485a57e1ca1ea5e9b00bda39f1
giovanniarmas02 <giovanniarmasd@gmail.com>
2026-09-01 10:19:57 +0100

    Version 1.

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+    add_result=a+b
+    return add_result
+
+def subtract(a,b):
+    sub_result=a-b
+    return sub_result
+
+def multiply(a,b):
+    mul_result=a*b
+    return mul_result
+
+def division(a,b):
+    try: 
+        div_result=a/b
+        return div_result
+    except ZeroDivisionError:
+        return "Error: cannot divide by 0."
+
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+operation=input("Choose operation(+, -, *, /): ")
+
+if operation=="+":
+    print(add(num1, num2))
+elif operation=="-":
+    print(subtract(num1, num2))
+elif operation=="*":
+    print(multiply(num1, num2))    
+elif operation=="/":
+    print(division(num1, num2))
