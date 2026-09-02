479bf44293acf68452bac48a49ef3621d9d82a9a
giovanniarmas02 <giovanniarmasd@gmail.com>
2026-09-02 09:54:37 +0100

    Version 1.2: added more operations and input validation.

--- Readme.md
+++ Readme.md
@@ -1,8 +1,15 @@
 # Python calculator
 
-a simple calculator project, built incrementally version by version.
+A simple calculator project, built incrementally version by version.
 
 ## Version 1
 - Takes 2 numbers and an operation (+, -, *, /).
 - Perfoms the operation.
 - Handles division by zero.
+
+## Version 1.1
+- While loop included.
+
+## Version 1.2
+- Added more kinds of operations (+, -, *, /, //, %, **).
+- Added input validation using try/except to handle invalid number input.
--- calculator.py
+++ calculator.py
@@ -17,19 +17,39 @@ def division(a,b):
     except ZeroDivisionError:
         return "Error: cannot divide by 0."
 
+def floor_division(a,b):
+    floor_div_result=a//b
+    return floor_div_result
+
+def modulo(a,b):
+    mod=a%b
+    return mod
+
+def exponentiation(a,b):
+    exp=a**b
+    return exp
+
 while True:
-    num1=float(input("Enter the first number: "))
-    num2=float(input("Enter the second number: "))
-    operation=input("Choose operation(+, -, *, /): ")
-    if operation=="+":
-        print(add(num1, num2))
-    elif operation=="-":
-        print(subtract(num1, num2))
-    elif operation=="*":
-        print(multiply(num1, num2))    
-    elif operation=="/":
-        print(division(num1, num2))
-    ask=input("Do you want to calculate again? (y/n)")
-    if ask !="y":
-        break
-    
+    try:
+        num1=float(input("Enter the first number: "))
+        num2=float(input("Enter the second number: "))
+        operation=input("Choose operation(+, -, *, /, //, %, **): ")
+        if operation=="+":
+            print(add(num1, num2))
+        elif operation=="-":
+            print(subtract(num1, num2))
+        elif operation=="*":
+            print(multiply(num1, num2))    
+        elif operation=="/":
+            print(division(num1, num2))
+        elif operation=="//":
+            print(floor_division(num1, num2))
+        elif operation=="%":
+            print(modulo(num1, num2))
+        elif operation=="**":
+            print(exponentiation(num1, num2))
+        ask=input("Do you want to calculate again? (y/n): ")
+        if ask !="y":
+            break
+    except ValueError:
+        print("That's not a valid number.")
